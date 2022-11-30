a27286e3f37ed1c1c19ff3ea98694110a088e4d8
Will Weihnacht <wwhynot@terpmail.umd.edu>
2022-11-30 15:29:09 -0800

    setup basic board html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view");// require appropriate file
+const Game = require("../ttt_node/game");// require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+
+  let ttt = document.querySelector(".ttt");
+  let g = new Game();
+  let v = new View(g,ttt);
 });
--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,7 +1,19 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    el.appendChild(this.setupBoard());
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    let parent = document.createElement("ul");
+    for (let i = 0; i < 9; i++) {
+      let child = document.createElement("li");
+      child.classList.add("square");
+      child.dataset.row = Math.floor(i/3);
+      child.dataset.col = (i%3);
+      parent.appendChild(child);
+    }
+    return parent;
+  }
   
   bindEvents() {}
 
